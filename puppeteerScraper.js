3d5d828910f7967c610e861150373e3bd4e4841e
lorenzo <lorenzotijerina@gmail.com>
2024-01-05 16:59:50 -0500

    use tester to run scrapeWithPuppeteer

--- puppeteerScraper.js
+++ puppeteerScraper.js
@@ -1,19 +1,20 @@
-const puppeteer = require("puppeteer");
+import puppeteer from "puppeteer";
 
 const scrapeWithPuppeteer = async (url) => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
+  console.log("Navigating to URL:", url);
   await page.goto(url, { waitUntil: "networkidle0" });
 
   const delay = (duration) =>
     new Promise((resolve) => setTimeout(resolve, duration));
 
-  await delay(3000); // Initial delay for page load
+  await delay(5000); // Initial delay for page load
 
   await page.evaluate(async () => {
     await new Promise((resolve) => {
       var totalHeight = 0;
-      var distance = 200; // Increased scrolling distance
+      var distance = 400; // scrolling distance
       var timer = setInterval(() => {
         var scrollHeight = document.body.scrollHeight;
         window.scrollBy(0, distance);
@@ -23,15 +24,15 @@ const scrapeWithPuppeteer = async (url) => {
           clearInterval(timer);
           resolve();
         }
-      }, 500); // Slower interval for scrolling
+      }, 500);
     });
   });
 
-  await delay(10000); // Additional delay after scrolling to ensure all posts are loaded
+  await delay(20000); // Additional delay after scrolling to ensure all posts are loaded
 
   const content = await page.content();
   await browser.close();
   return content;
 };
 
-module.exports = scrapeWithPuppeteer;
+export default scrapeWithPuppeteer;
--- puppeteerTester.js
+++ puppeteerTester.js
@@ -1,21 +1,17 @@
-import("dotenv").then((dotenv) => dotenv.config());
+import dotenv from "dotenv";
+dotenv.config();
 
-import { launch } from "puppeteer";
+import scrapeWithPuppeteer from "./puppeteerScraper.js";
 
 const testPuppeteer = async () => {
-  const browser = await launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto(process.env.WIX_BLOG_URL, { waitUntil: "networkidle0" });
+  try {
+    const url = process.env.WIX_BLOG_URL;
+    const content = await scrapeWithPuppeteer(url);
 
-  // Close the browser after a delay
-  setTimeout(async () => {
-    await browser.close();
-  }, 10000);
+    console.log("Scraped content:", content);
+  } catch (error) {
+    console.error("Error in puppeteerTester:", error);
+  }
 };
 
-testPuppeteer().catch((error) => {
-  console.error("Error in puppeteerTester:", error);
-  process.exit(1);
-});
-
-export default testPuppeteer;
+testPuppeteer();

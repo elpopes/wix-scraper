cbcd26f022bdd809d2a6250874d215e0d6369476
lorenzo <lorenzotijerina@gmail.com>
2024-01-19 11:34:03 -0500

    use cheerio for additional contentCleaner

--- contentCleaner.js
+++ contentCleaner.js
@@ -1,35 +1,31 @@
+import cheerio from "cheerio";
+
 export const cleanContent = (htmlContent) => {
+  const $ = cheerio.load(htmlContent);
+
   // Remove all style and script tags
-  htmlContent = htmlContent.replace(/<style.*?>.*?<\/style>/gis, "");
-  htmlContent = htmlContent.replace(/<script.*?>.*?<\/script>/gis, "");
+  $("style, script").remove();
 
-  // Convert spans with italic styling to <em>
-  htmlContent = htmlContent.replace(
-    /<span[^>]*style=["'][^"']*font-style:\s*italic[^"']*["'][^>]*>((.|\n)*?)<\/span>/gis,
-    "<em>$1</em>"
-  );
+  // Iterate over each element and process accordingly
+  $("*").each(function () {
+    const element = $(this);
 
-  // Remove nested <span> tags within <em> tags
-  htmlContent = htmlContent.replace(
-    /<em>((.|\n)*?)<\/span><\/em>/gis,
-    "<em>$1</em>"
-  );
+    // Convert specific style spans to <em>
+    if (
+      element.is("span") &&
+      element.attr("style") &&
+      element.attr("style").includes("font-style: italic")
+    ) {
+      element.replaceWith($("<em>").html(element.html()));
+    }
 
-  // Remove inline styles and class attributes
-  htmlContent = htmlContent.replace(
-    /<span[^>]*style=["'][^"']*["'][^>]*>((.|\n)*?)<\/span>/gis,
-    "$1"
-  );
-  htmlContent = htmlContent.replace(
-    /<span[^>]*class=["'][^"']*["'][^>]*>((.|\n)*?)<\/span>/gis,
-    "$1"
-  );
+    // Remove style and class attributes
+    element.removeAttr("style");
+    element.removeAttr("class");
 
-  // Remove all remaining span tags but keep their content
-  htmlContent = htmlContent.replace(/<span[^>]*>((.|\n)*?)<\/span>/gis, "$1");
+    // Other specific processing can be added here
+  });
 
-  // Optionally, convert HTML entities like &nbsp; to regular spaces
-  htmlContent = htmlContent.replace(/&nbsp;/gi, " ");
-
-  return htmlContent;
+  // Return the cleaned HTML
+  return $("body").html() || "";
 };
